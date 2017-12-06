c160982c80f748e7e05c1ff49fd5c3c76b6986d1
David <David@DavidPC>
2017-12-06 20:54:20 +0100

    Modificacion en master

---
+++ Hilos_Uno/src/com/david/Constantes.java
@@ -0,0 +1,11 @@
+package com.david;
+
+public class Constantes {
+	
+	public static final String CTES_UNO  = "uno";
+	public static final String CTES_DOS  = "dos";
+	public static final String CTES_TRES = "tres";	
+	
+	public static final String CTES_FIN  = "Final del programa";
+
+}
--- Hilos_Uno/src/com/david/ThreadsExecutor.java
+++ Hilos_Uno/src/com/david/ThreadsExecutor.java
@@ -4,15 +4,15 @@ public class ThreadsExecutor {
 
 	public static void main(String[] args) {
 
-		Thread dm1 = new Thread(new DemoThread("uno"));
-		Thread dm2 = new Thread(new DemoThread("dos"));
-		Thread dm3 = new Thread(new DemoThread("tres"));
+		Thread dm1 = new Thread(new DemoThread(Constantes.CTES_UNO));
+		Thread dm2 = new Thread(new DemoThread(Constantes.CTES_DOS));
+		Thread dm3 = new Thread(new DemoThread(Constantes.CTES_TRES));
 		
 		dm1.start();
 		dm2.start();
 		dm3.start();
 		
-		System.out.println("Final del programa");
+		System.out.println(Constantes.CTES_FIN);
 
 	}
 
