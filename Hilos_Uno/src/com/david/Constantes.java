971c7cd90af38eb093879d688c928ec477106587
david.garcia.sanz@gmail.com <david.garcia-sanz@gfi.world>
2019-10-02 17:26:12 +0200

    comentario en constantes

--- Hilos_Uno/src/com/david/Constantes.java
+++ Hilos_Uno/src/com/david/Constantes.java
@@ -1,7 +1,7 @@
 package com.david;
 
 public class Constantes {
-	
+	// constantes
 	public static final String CTES_UNO  = "uno";
 	public static final String CTES_DOS  = "dos";
 	public static final String CTES_TRES = "tres";	
